98d11525109a21a8e1b476127aae4c34b68fea39
Marina Hoashi <marinahoashi@gmail.com>
2017-04-20 14:49:54 -0400

    Optimized merge sort performance

--- mergesort/mergesort.js
+++ mergesort/mergesort.js
@@ -8,26 +8,32 @@ function split(wholeArray) {
 
 function merge(arr1, arr2) {
   var outputArr = [];
-  while(arr1.length || arr2.length){
-    if(!arr1.length){
-      outputArr = outputArr.concat(arr2);
-      arr2 = [];
-    } else if (!arr2.length) {
-      outputArr = outputArr.concat(arr1);
-      arr1 = [];
-    } else if(arr1[0] < arr2[0]){
-      outputArr = outputArr.concat(arr1[0]);
-      arr1.shift();
+  var arr1Idx = 0;
+  var arr2Idx = 0;
+  var endOfOutputIdx = 0;
+
+  while (arr1Idx < arr1.length || arr2Idx < arr2.length) {
+    endOfOutputIdx = outputArr.length;
+
+    if (arr1Idx === arr1.length) {
+      outputArr = outputArr.concat(arr2.slice(arr2Idx));
+      arr2Idx = arr2.length;
+    } else if (arr2Idx === arr2.length) {
+      outputArr = outputArr.concat(arr1.slice(arr1Idx));
+      arr1Idx = arr1.length;
+    } else if (arr1[arr1Idx] < arr2[arr2Idx]) {
+      outputArr[endOfOutputIdx] = arr1[arr1Idx];
+      arr1Idx++;
     } else {
-      outputArr = outputArr.concat(arr2[0]);
-      arr2.shift();
+      outputArr[endOfOutputIdx] = arr2[arr2Idx];
+      arr2Idx++;
     }
   }
   return outputArr;
 }
 
 function mergeSort(array) {
-  if(array.length <= 1) return array;
+  if (array.length <= 1) return array;
   var splitArrays = split(array);
   var leftSideSorted = mergeSort(splitArrays[0]);
   var rightSideSorted = mergeSort(splitArrays[1]);
